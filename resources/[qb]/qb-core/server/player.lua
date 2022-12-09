721e91b27b1f810cf6fb1d47e5c1f1a28a3e8b9b
tichys <tichman27@gmail.com>
2022-12-09 07:56:51 +0100

    why am i retarded?

--- resources/[qb]/qb-core/server/player.lua
+++ resources/[qb]/qb-core/server/player.lua
@@ -123,9 +123,9 @@ function QBCore.Player.CheckPlayerData(source, PlayerData)
     PlayerData.metadata['licences'] = PlayerData.metadata['licences'] or {
         ['driver'] = true,
         ['business'] = false,
-        ['weapon'] = false
+        ['weapon'] = false,
         ['fly_permit'] = false,
-        ['fly'] = false,
+        ['fly'] = false
     }
     PlayerData.metadata['inside'] = PlayerData.metadata['inside'] or {
         house = nil,
