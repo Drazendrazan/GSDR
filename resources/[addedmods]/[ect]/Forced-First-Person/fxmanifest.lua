0a5bc9680498571e38c62f9e2b327846973b4e4a
tichys <tichman27@gmail.com>
2022-12-10 07:33:14 +0100

    more updates

---
+++ resources/[addedmods]/[ect]/Forced-First-Person/fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'vipex#2027'
+description 'Forced First Person with Multiple Options.'
+version '2.0'
+
+client_script 'client.lua'
+server_script 'config.lua'
+shared_script 'config.lua'
