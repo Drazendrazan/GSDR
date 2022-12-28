8152464102e8513d52dc8501f4042ed2a662ed00
tichys <tichman27@gmail.com>
2022-12-28 09:03:57 +0100

    updated pathing

--- resources/[standalone]/cdn-fool/fxmanifest.lua
+++ resources/[standalone]/cdn-fool/fxmanifest.lua
@@ -1,7 +1,7 @@
 fx_version 'cerulean'
 game 'gta5'
 author 'https://www.github.com/CodineDev' -- Base Refueling System from PS (https://www.github.com/Project-Sloth), other code by CodineDev (https://www.github.com/CodineDev). 
-description 'cdn-fuel'
+description 'cdn-fool'
 version '2.0.0'
 
 data_file 'DLC_ITYP_REQUEST' 'stream/[electric_nozzle]/electric_nozzle_typ.ytyp'
