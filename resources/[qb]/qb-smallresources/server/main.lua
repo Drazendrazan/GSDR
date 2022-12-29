db4a25026fd084b19beb2874eb91256ebbdef46d
tichys <tichman27@gmail.com>
2022-12-29 11:09:28 +0100

    TAX UPDATES

--- resources/[qb]/qb-smallresources/server/main.lua
+++ resources/[qb]/qb-smallresources/server/main.lua
@@ -72,3 +72,83 @@ QBCore.Functions.CreateCallback('smallresources:server:GetCurrentPlayers', funct
     end
     cb(TotalPlayers)
 end)
+
+function LightDrunkEffect()
+    local player = PlayerPedId()
+    Wait(3000)
+    DoScreenFadeOut(1000)
+    Wait(1000)
+    SetTimecycleModifier('spectator5')
+    SetPedMotionBlur(PlayerPedId(), true)
+    SetPedMovementClipset(PlayerPedId(), 'MOVE_M@DRUNK@SLIGHTLYDRUNK', true)
+    SetPedIsDrunk(PlayerPedId(), true)
+    SetPedAccuracy(PlayerPedId(), 0)
+    DoScreenFadeIn(1000)
+    if IsPedRunning(player) then
+        SetPedToRagdoll(player, math.random(1000, 2000), math.random(1000, 2000), 3, 0, 0, 0)
+    end
+    Wait(2000)
+    if (60 >= math.random(1,100)) and IsPedRunning(player) then
+        SetPedToRagdollWithFall(ped, 2500, 4000, 1, GetEntityForwardVector(ped), 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
+    end            
+    Wait(5*60000) -- 5 mins
+    if IsPedRunning(player) then
+        SetPedToRagdoll(player, math.random(1000, 2000), math.random(1000, 2000), 3, 0, 0, 0)
+    end
+    Wait(2000)
+    if (30 >= math.random(1,100)) and IsPedRunning(player) then
+        SetPedToRagdollWithFall(ped, 1500, 3000, 1, GetEntityForwardVector(ped), 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
+    end
+    DoScreenFadeOut(1000)
+    Wait(1000)
+    DoScreenFadeIn(1000)
+    ClearTimecycleModifier()
+    ResetScenarioTypesEnabled()
+    ResetPedMovementClipset(PlayerPedId(), 0)
+    SetPedIsDrunk(PlayerPedId(), false)
+    SetPedMotionBlur(PlayerPedId(), false)
+end
+
+function HeavyDrunkEffect()
+    local ped = PlayerPedId()
+    if IsPedWalking(player) or IsPedRunning(player) then
+        SetPedToRagdollWithFall(ped, 2500, 4000, 1, GetEntityForwardVector(ped), 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
+    end
+    Wait(5000)
+    DoScreenFadeOut(1500)
+    SetFlash(0, 0, 500, 7000, 500)
+    ShakeGameplayCam('LARGE_EXPLOSION_SHAKE', 1.00)
+    Wait(2000)
+    SetTimecycleModifier('spectator5')
+    SetPedMotionBlur(PlayerPedId(), true)
+    SetPedMovementClipset(PlayerPedId(), 'MOVE_M@DRUNK@VERYDRUNK', true)
+    SetPedIsDrunk(PlayerPedId(), true)
+    SetPedAccuracy(PlayerPedId(), 0)
+    SetFlash(0, 0, 500, 7000, 500)
+    ShakeGameplayCam('DRUNK_SHAKE', 1.10)
+    Wait(2000)
+    DoScreenFadeIn(1800)
+    if IsPedWalking(player) or IsPedRunning(player) then
+        SetPedToRagdollWithFall(ped, 2500, 4000, 1, GetEntityForwardVector(ped), 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
+    end
+    SetFlash(0, 0, 500, 7000, 500)
+    ShakeGameplayCam('SMALL_EXPLOSION_SHAKE', 1.20)
+    Wait(2000)            
+    Wait(10*60000) -- 10 mins
+    DoScreenFadeOut(1400)
+    SetFlash(0, 0, 500, 7000, 500)
+    ShakeGameplayCam('DRUNK_SHAKE', 1.10)
+    Wait(2000)  
+    DoScreenFadeIn(1200)
+    if IsPedWalking(player) or IsPedRunning(player) then
+        SetPedToRagdollWithFall(ped, 2500, 4000, 1, GetEntityForwardVector(ped), 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
+    end
+    SetFlash(0, 0, 500, 7000, 500)
+    ShakeGameplayCam('SMALL_EXPLOSION_SHAKE', 1.05)
+    Wait(1000)
+    ClearTimecycleModifier()
+    ResetScenarioTypesEnabled()
+    ResetPedMovementClipset(PlayerPedId(), 0)
+    SetPedIsDrunk(PlayerPedId(), false)
+    SetPedMotionBlur(PlayerPedId(), false)
+end
