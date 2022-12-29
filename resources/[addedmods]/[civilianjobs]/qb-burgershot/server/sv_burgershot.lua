d2c9ddcea9fd69eb9c71cdd2613d893641a88b0d
tichys <tichman27@gmail.com>
2022-12-29 17:46:53 +0100

    FUCK ME WITH THIS SHIT

--- resources/[addedmods]/[civilianjobs]/qb-burgershot/server/sv_burgershot.lua
+++ resources/[addedmods]/[civilianjobs]/qb-burgershot/server/sv_burgershot.lua
@@ -1,5 +1,16 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
+local function HasItem(pid, item)
+    if Config.OxInventory == 'started' then
+        return exports.ox_inventory:Search(pid, 'count', item)
+    elseif Config.Framework == 'qb-core' then
+        return QBCore.Functions.HasItem(pid, item, 1)
+    elseif Config.Framework == 'esx' then
+        local Player = ESX.GetPlayerFromId(pid)
+        return Player.hasItem(item)
+    end
+end
+
 RegisterNetEvent('qb-burgershot:server:makeBleeder', function()
     local Player = QBCore.Functions.GetPlayer(source)
     if not Player then return end
