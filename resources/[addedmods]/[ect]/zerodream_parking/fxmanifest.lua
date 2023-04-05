7552ccf459944afd3cf9d08e7d21755921ca1d98
Tich none <tichman27@gmail.com>
2023-04-05 18:48:24 +0200

    mom i need help

---
+++ resources/[addedmods]/[ect]/zerodream_parking/fxmanifest.lua
@@ -0,0 +1,26 @@
+fx_version  'cerulean'
+games       { 'gta5' }
+
+name        'zerodream_parking'
+author      'Akkariin'
+description 'ZeroDream Parking Script'
+version     '1.0.1'
+url         'https://www.zerodream.net/'
+
+shared_scripts {
+    'config.lua',
+    'locales/*.lua',
+}
+
+client_scripts {
+    'client/utils.lua',
+    'client/api.lua',
+    'client/main.lua',
+}
+
+server_scripts {
+    '@mysql-async/lib/MySQL.lua',
+    'server/utils.lua',
+    'server/api.lua',
+    'server/main.lua',
+}
